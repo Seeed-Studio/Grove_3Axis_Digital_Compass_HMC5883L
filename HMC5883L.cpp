b350abdff5f4301c4771c4b06b9f16b2926f3b36
Dirk-Willem van Gulik <dirkx@webweaving.org>
2018-08-14 10:13:44 +0200

    Cut the comparisons a bit more slack.

--- HMC5883L.cpp
+++ HMC5883L.cpp
@@ -120,42 +120,48 @@ MagnetometerScaled HMC5883L::readScaledAxis()
 short HMC5883L::setScale(float gauss)
 {
     uint8_t regValue = 0x00;
-    if(gauss == 0.88)
+
+/* Some of these values; e.g. 1.3 - cause comparison
+ * issues with the compiler that the Arduino IDE uses.
+ */
+#define CLOSEENOUGH(x,y) (fabs(x-y)<0.001)
+
+    if(CLOSEENOUGH(gauss , 0.88))
     {
         regValue = 0x00;
         m_Scale = 0.73;
     }
-    else if(gauss == 1.3)
+    else if(CLOSEENOUGH(gauss , 1.3))
     {
         regValue = 0x01;
         m_Scale = 0.92;
     }
-    else if(gauss == 1.9)
+    else if(CLOSEENOUGH(gauss , 1.9))
     {
         regValue = 0x02;
         m_Scale = 1.22;
     }
-    else if(gauss == 2.5)
+    else if(CLOSEENOUGH(gauss , 2.5))
     {
         regValue = 0x03;
         m_Scale = 1.52;
     }
-    else if(gauss == 4.0)
+    else if(CLOSEENOUGH(gauss , 4.0))
     {
         regValue = 0x04;
         m_Scale = 2.27;
     }
-    else if(gauss == 4.7)
+    else if(CLOSEENOUGH(gauss , 4.7))
     {
         regValue = 0x05;
         m_Scale = 2.56;
     }
-    else if(gauss == 5.6)
+    else if(CLOSEENOUGH(gauss , 5.6))
     {
         regValue = 0x06;
         m_Scale = 3.03;
     }
-    else if(gauss == 8.1)
+    else if(CLOSEENOUGH(gauss , 8.1))
     {
         regValue = 0x07;
         m_Scale = 4.35;
